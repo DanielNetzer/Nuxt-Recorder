02caa35f8eef04c9709260bc8007be548ff8baf4
Michael Liberman <michael.liberman@iamplus.com>
2018-09-13 09:40:32 +0300

    - fixing db connection bug

--- api/index.js
+++ api/index.js
@@ -4,8 +4,11 @@ const mongoose = require('mongoose');
 //Set up mongoose connection
 // TODO: Talk with Michael, unable to connect to mongoose!
 // const mongoConnString = 'mongodb://sensiya:P@ssw0rd@54.196.118.71:27017/recordings';
-mongoose.connect('mongodb://172.131.7.56:27017/recordings', { user: 'sensiya', pass: 'P@ssw0rd' }, (err) => {
-    throw new Error(`MongoDB Error, ${err}`);
+mongoose.connect('mongodb://127.0.0.1:27017/recordings', { user: 'sensiya', pass: 'P@ssw0rd' }, (err) => {
+    if (err) {
+        throw new Error(`MongoDB Error, ${err}`);
+    }
+
 });
 
 mongoose.connection.on('error', (err) => {
