304fe0a1f333bd572709ed7129bcb4da156a3ea0
Michael Liberman <michael.liberman@iamplus.com>
2018-09-13 09:11:12 +0300

    - changed the mongo ip in connection string

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 //Set up mongoose connection
 // TODO: Talk with Michael, unable to connect to mongoose!
 // const mongoConnString = 'mongodb://sensiya:P@ssw0rd@54.196.118.71:27017/recordings';
-mongoose.connect('mongodb://54.196.118.71:27017/recordings', { user: 'sensiya', pass: 'P@ssw0rd' },
+mongoose.connect('mongodb://172.131.7.56:27017/recordings', { user: 'sensiya', pass: 'P@ssw0rd' },
     (err) => { console.error(err); });
 
 mongoose.connection.on('error', () => {
