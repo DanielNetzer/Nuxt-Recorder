cdf7f08cf8b8a1e25c5d87e16f9ae579c58bf42e
DanielNetzer <daniel.netzer7@gmail.com>
2018-09-13 09:18:22 +0300

    updated build process.

--- api/index.js
+++ api/index.js
@@ -4,11 +4,12 @@ const mongoose = require('mongoose');
 //Set up mongoose connection
 // TODO: Talk with Michael, unable to connect to mongoose!
 // const mongoConnString = 'mongodb://sensiya:P@ssw0rd@54.196.118.71:27017/recordings';
-mongoose.connect('mongodb://54.196.118.71:27017/recordings', { user: 'sensiya', pass: 'P@ssw0rd' },
-    (err) => { console.error(err); });
+mongoose.connect('mongodb://172.131.7.56:27017/recordings', { user: 'sensiya', pass: 'P@ssw0rd' }, (err) => {
+    throw new Error(`MongoDB Error, ${err}`);
+});
 
-mongoose.connection.on('error', () => {
-    throw new Error(`unable to connect to database: ${mongoConnString}`);
+mongoose.connection.on('error', (err) => {
+    throw new Error(`MongoDB Error, ${err}`);
 });
 
 // Create express instnace
